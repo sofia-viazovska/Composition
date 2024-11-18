377ecea8d1531e760c28173750b6c96b7c297cad
sofia_v <sofi.sonia2609@gmail.com>
2024-11-18 13:19:18 +0200

    1st task ok 2nd task Error: f.on is not a function

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const compose = (...fns) => (x) => null;
+const compose = (...fns) => (x) => {
+  const holder = [];
+  const composed = (x) => {
+    if (fns.length === 0) return x;
+    const n = fns.length - 1;
+    let res = x;
+    try {
+      for (let i = n; i >= 0; i--) {
+        res = fns[i](res);
+      }
+      return res;
+    } catch (error) {
+      for (const i of holder) {
+        i(error);
+      }
+      return null;
+    }
+  };
+  composed.on = (name, holder) => {
+    if (name === 'error') holder.push(i);
+  };
+  return composed;
+};
 
 module.exports = { compose };
